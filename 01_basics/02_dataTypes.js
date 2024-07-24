6417aebe59084b3e6981117b47b0140e4868c5e8
Anant Gabhane <72884480+AnantGabhane@users.noreply.github.com>
2024-07-24 06:25:24 +0000

    datatypes learning

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+"use strict"; //newer version of JS
+
+// number 
+// BigInt
+// String
+// Boolean
+// null
+// undefined
+// Symbol - uniqueness of componenet
+// 
+// Object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
