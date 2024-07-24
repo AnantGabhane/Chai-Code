c198ef5405c7664674b42d23a090518978f38c24
Anant Gabhane <72884480+AnantGabhane@users.noreply.github.com>
2024-07-24 06:25:12 +0000

    variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,7 +1,8 @@
 const accountId = 1333  // will be constant abd will not change
-let accountEmail = "anant@gmail.com"
-var accountPassword = "12345"
+let accountEmail = "anant@gmail.com" // get rid of scope problem
+var accountPassword = "12345" // we do not use var in modern dev due to blocked and functional scope 
+
 accountCity = "shegaon"
 
-console.log(acc)
-
+// console.log(accountEmail)
+console.table([accountId,accountEmail,accountPassword,accountCity])
