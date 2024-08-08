6929fd4771c770b6e0e4acf70fd2dd134faafdd4
Anant Gabhane <72884480+AnantGabhane@users.noreply.github.com>
2024-08-08 04:37:05 +0000

    Conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+let score = "30abc"
+/*
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+*/
+
+// number
+// NaN
+//string - NaN
+//undefined - NaN
+//null - 0
+//true - 1
+//false - 0
+// empty string bool is false
+// "anant" - true
+
+
+let isLoggedIn = ""
+let booleanIsloggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsloggedIn);
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber); //33
+console.log(typeof stringNumber); //number
+
+// ------------------prefix ---------------------
+//values incremented before useage
+
+
+// ------------------postfix ---------------------
+//values incremented after useage
