549c7f2c5fb9ea09b8a6cfdf7e8e949bfbaf7f55
Anant Gabhane <72884480+AnantGabhane@users.noreply.github.com>
2024-08-23 08:06:57 +0000

    comparision in JS

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,9 @@
+// TS provides you with extra strict rules to work with datatypes
+// TS doesn't allow you two compare two different datatypes
+// two comparision entites must contain same data type
+
+// console.log("2" > 1);
+
+// In js we do not get predictable results we often get some different rusults
+
+// In js comparision and equality operators work differently
