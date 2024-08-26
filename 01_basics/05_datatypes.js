a7327a8b74be2ef8d79e7acf89a31a82830afe68
Anant Gabhane <72884480+AnantGabhane@users.noreply.github.com>
2024-08-26 06:12:04 +0000

    js: datatypes

---
+++ 01_basics/05_datatypes.js
@@ -0,0 +1,49 @@
+/*
+datatypes 
+
+- primitive -> call by value change occure in copy 
+
+    types : string, number, bool, null, undefined, symbol (to make anything unique used for a component), Bigint
+
+- reference (Non primitives) - ref in memory is directly allocated
+    
+    array, objects , functions
+    to master js - master objects and brower events or web events master these 
+
+    array
+        const hero = ["ironman","hulk","cap","thor"]
+
+    objects 
+    let myObjects = 
+        {
+            name: "anant",
+            age:23,
+
+        }
+    
+    function
+    const myFunction =
+        function(){}
+    return type of function is << object function >>
+*/ 
+/*
+Return type of variables in JavaScript 
+1) Primitive Datatypes        
+    Number => number        
+    String  => string        
+    Boolean  => boolean        
+    null  => object        
+    undefined  =>  undefined        
+    Symbol  =>  symbol        
+    BigInt  =>  bigint 
+
+2) Non-primitive Datatypes        
+    Arrays  =>  object        
+    Function  =>  function        
+    Object  =>  object
+ 
+*/
+
+// const score:number = 100 - types safety is good coding practice
+
+// how to check datatype of any varible or value - typeof ___variable___
