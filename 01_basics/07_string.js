f53969c07e12f30b684c048acc480528ad2640e7
Anant Gabhane <72884480+AnantGabhane@users.noreply.github.com>
2024-09-05 06:27:53 +0000

    all about strings

---
+++ 01_basics/07_string.js
@@ -0,0 +1,18 @@
+const name = "anant"
+const repoCount = 50
+// string interpolation -- fstring in python 
+console.log(`Hey there, I'm ${name} and my repoCount is ${repoCount}`);
+
+const gameName = new String('anant')
+console.log(gameName[0]); //a
+console.log(gameName.length); // 5
+console.log(gameName.toUpperCase()); //ANANT
+
+const newString = "     asdasda    "
+console.log(newString); 
+console.log(newString.trim()); //asdasda
+
+// .replace(" ", " ")
+
+// .includes(' ')
+
