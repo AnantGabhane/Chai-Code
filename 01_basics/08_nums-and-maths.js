5e3910ebcf8333cfb3fd7a48e628c7677768b196
Anant Gabhane <72884480+AnantGabhane@users.noreply.github.com>
2024-09-22 13:59:01 +0000

    numbers and math

---
+++ 01_basics/08_nums-and-maths.js
@@ -0,0 +1,12 @@
+const score = 100
+// console.log(score);
+
+const balence = new Number(100)
+// console.log(balence);
+//here number is specially casted to be a number 
+
+// console.log(balence.toString());
+// console.log(balence.toFixed(2)); // used in commerese application for precision
+
+// ----Maths----
+
