c0cc7b45f7ff0f738e56996f4e9f738b653677f3
Anant Gabhane <72884480+AnantGabhane@users.noreply.github.com>
2024-09-22 14:10:11 +0000

    time module

---
+++ 01_basics/09_datesin.js
@@ -0,0 +1,8 @@
+// data is a object that represents single moment in time independent of format.
+
+// date object - instance 
+
+let myDate = new Date()
+console.log(myDate); //2024.dmz log unreadeable output 
+console.log(myDate.toDateString()); //sun sep 
+console.log(typeof myDate); // object 
