539562da94e401e21127c7d4fc523ead38ba7fd5
Anant Gabhane <72884480+AnantGabhane@users.noreply.github.com>
2024-09-23 19:56:16 +0000

    objects basics

---
+++ 03_objects/01_basics.js
@@ -0,0 +1,19 @@
+// In js, master 2 topics, objects and events 
+
+//  2 ways to define a object 
+//      1. Literal -> cannot create a singleton object
+//      2. constructor -> create a singleton object everytime, have multiple instances of same object
+
+// singleton is a unique instance of it;s type 
+
+// defining a object - it;s basically a key value pair
+const jsuser = {
+    name : "anant",
+    surname :"gabhane",
+    age: 24,
+    isLogged :false
+}
+
+// accessing the object values
+console.log(jsuser.age);
+console.log(jsuser[age]);
