c37187207577111eed96e7e9499563dfc472a1ce
Anant Gabhane <72884480+AnantGabhane@users.noreply.github.com>
2024-09-23 19:56:16 +0000

    objects basics

--- 03_objects/01_basics.js
+++ 03_objects/01_basics.js
@@ -7,27 +7,17 @@
 // singleton is a unique instance of it;s type 
 
 // defining a object - it;s basically a key value pair
-<<<<<<< HEAD
-=======
 const mySym = Symbol("key1")
 
->>>>>>> af584aa (objects basics)
 const jsuser = {
     name : "anant",
     surname :"gabhane",
     age: 24,
-<<<<<<< HEAD
-=======
     [mySym] :"key1111",
->>>>>>> af584aa (objects basics)
     isLogged :false
 }
 
 // accessing the object values
-<<<<<<< HEAD
-console.log(jsuser.age);
-console.log(jsuser[age]);
-=======
 // console.log(jsuser.age); // 24
 // console.log(jsuser["age"]); //24
 
@@ -52,4 +42,3 @@ jsuser.greeting2 = function(){
     console.log(`hey js user ${this.name}`);
 }
 console.log(jsuser.greeting2());
->>>>>>> af584aa (objects basics)
