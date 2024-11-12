005bc6af03928bdb01863357bc397a96897bbf76
Anant Gabhane <72884480+AnantGabhane@users.noreply.github.com>
2024-11-12 17:21:44 +0000

    operatros

---
+++ 05_Control flow/01_basics.js
@@ -0,0 +1,36 @@
+/*
+Control flow - some part of code should work based on login and some should work on logout- this will be dictated by control flow
+
+*/
+
+
+//if - condition is a comnparision  
+// if (condition) {
+    
+// }
+
+// == this will check for value of these 2 values are equal
+
+// === this will check for values of 2 elements and it's type are also equal 
+
+
+// if (condition) {
+    
+// } else {
+    
+// }
+
+// && - AND condition -> all the conditions should be true 
+
+// || - OR confition -> one of the condition should be true 
+
+// break - breaks the flow of code 
+
+
+// check if object is empty or not 
+
+const emptyObj = {}
+if (Object.keys(emptyObj).length === 0) {
+    console.log("object is empty");
+     
+}
