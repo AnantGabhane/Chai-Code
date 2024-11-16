c7256f9e20f4863dc8f2a2290f5722d42bc939a1
Anant Gabhane <72884480+AnantGabhane@users.noreply.github.com>
2024-11-16 13:52:19 +0000

    While loop

---
+++ 06_iterations/While_dowhile_loop.js
@@ -0,0 +1,10 @@
+let index = 1
+// while (index <=10) {
+//     // console.log(`Value of index ${index}`);
+//     index++; // loop termination condition to exit the loop so that i don;t become a infinite loop
+// }
+
+do {
+    console.log(`index is ${index}`);
+    index++
+} while (index<=10);
