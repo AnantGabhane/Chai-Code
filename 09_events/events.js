524d3a39c6f584d74c433cb21a26dd60d6368344
Anant Gabhane <anant.gabhane@apexaiq.com>
2025-03-10 17:31:21 +0530

    events

---
+++ 09_events/events.js
@@ -0,0 +1,9 @@
+/* This file contains infomation related to events in js 
+
+- All events in JS run sequentially
+
+- JS is sequentially run language to avaid this we use async functions to deviate tasks
+
+- Events are invoked when activity is done
+
+*/
